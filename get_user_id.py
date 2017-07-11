d32a8e15654b088e6a9aa9c4750a2163fb62905e
Manish <khiladimanu1@gmail.com>
2017-07-11 22:51:10 +0530

    get_user_id File is Updated

---
+++ get_user_id.py
@@ -0,0 +1,22 @@
+# Files and Functions are Imported
+
+import requests
+from constant_variable import APP_ACCESS_TOKEN ,BASE_URL
+
+def get_user_id(insta_username):
+    # Function Logic to get user id
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print( user_info['data'][0]['id'])
+            return user_info['data'][0]['id']
+        else:
+            return None
+    else:
+        print '\n\t\t*****Status code other than 200 received!*****'
+        exit()
+
+#get_user_id(insta_username='imanuchauhan')
